f411c43cd3bde75fc529a6bbb3d8c07f15fcf371
DDOwzw <zwwang0816@gmail.com>
2019-03-02 21:17:13 -0600

    Life Cycle Playground

---
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -0,0 +1,58 @@
+package com.laioffer.tinnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.laioffer.tinnews.common.TinBasicActivity;
+
+public class MainActivity extends TinBasicActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView view = findViewById(R.id.text);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void showSnackBar(String message) {
+
+    }
+}
